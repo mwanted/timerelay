7b0162f94f52467c47c5465903ab2d6a23ed71ad
Gleb Belyakov <rem@mwnt.ru>
2022-09-28 22:43:46 +0300

    PinChange mode, variable channels count

--- sketch/timerelay.ino
+++ sketch/timerelay.ino
@@ -2,11 +2,12 @@
 // ***1. Удобоваримо выдавать текущее состояние, включакя оставшееся время.
 // ***2. Переписать чтение команд на неблокитующий способ. Собирать строку до возникновения перевода строки и потом ее анализировать.
 // 3. Сделать регулируемый "таймаут" на срабатывание, чтобы исключить включение на коротком свете.
-// ???4. Предусмотреть несколько каналаов.
+// ***4. Предусмотреть несколько каналаов.
 // ***5. Убрать настройки в массив структур с разделением по каналам. 
-// 6. Предусмотреть режим "pin-change"
+// ???6. Предусмотреть режим "pin-change"
 
 #define CHANNELS 2
+#define CYCLES 10
 #define BUFFLEN 20
 #define INITFLAG 0x10
 #define INITDATA 0xFA
@@ -22,20 +23,40 @@ struct CHANNEL {
 struct STATE {
   unsigned int on_delay, off_delay;
   uint8_t mode;
+  unsigned int count;
   unsigned long timemillis;
 };
 
 volatile CHANNEL settings[CHANNELS];
 volatile STATE state[CHANNELS];
-volatile bool debug;
+volatile byte pin_mask = 0;
 
 void printSettings() {
   char buffer[40];
   for (int i = 0; i < CHANNELS; i++) {
-    sprintf(buffer,"%i: on_delay:  %i",i,settings[i].on_delay); Serial.println(buffer);
-    sprintf(buffer,"%i: off_delay: %i",i,settings[i].off_delay); Serial.println(buffer);
-    sprintf(buffer,"%i: mode:      %i",i,settings[i].mode); Serial.println(buffer);
+    sprintf(buffer,"%c: on_delay:  %i",'A'+i,settings[i].on_delay); Serial.println(buffer);
+    sprintf(buffer,"%c: off_delay: %i",'A'+i,settings[i].off_delay); Serial.println(buffer);
+    sprintf(buffer,"%c: mode:      %i",'A'+i,settings[i].mode); Serial.println(buffer);
   }
+}
+
+void printState() {
+  char buffer[40];
+  for (int i = 0; i < CHANNELS; i++) {
+    sprintf(buffer,"%c: on_delay:  %i",'A'+i,state[i].on_delay); Serial.println(buffer);
+    sprintf(buffer,"%c: off_delay: %i",'A'+i,state[i].off_delay); Serial.println(buffer);
+    sprintf(buffer,"%c: mode:      %i",'A'+i,state[i].mode); Serial.println(buffer);
+    sprintf(buffer,"%c: count:     %i",'A'+i,state[i].count); Serial.println(buffer);
+  }  
+}
+
+byte bits(byte exponent)
+{
+    byte product = 0;
+    while (exponent--) {
+      product |= 1 << exponent;
+    }
+    return product;
 }
 
 bool parseInteger(unsigned int *data, char *chars) {
@@ -59,15 +80,6 @@ bool parseMode(byte *data, char mode) {
       return true;
   }
   return false;
-}
-
-void printState() {
-  char buffer[40];
-  for (int i = 0; i < CHANNELS; i++) {
-    sprintf(buffer,"%i: on_delay:  %i",i,state[i].on_delay); Serial.println(buffer);
-    sprintf(buffer,"%i: off_delay: %i",i,state[i].off_delay); Serial.println(buffer);
-    sprintf(buffer,"%i: mode:      %i",i,state[i].mode); Serial.println(buffer);
-  }  
 }
 
 bool parseGo(byte channel, char command) {
@@ -103,7 +115,7 @@ bool parseCommand(char *command) {
       writeEEPROM();
       return true;
     case 'B':
-      debug = !debug;
+//      debug = !debug;
       return true;
     case 'G':
       return (channel < CHANNELS) && parseGo(channel,command[2]);
@@ -138,12 +150,13 @@ void commandif() {
   char incomingChar;
   if (Serial.available() > 0) {
     incomingChar = Serial.read();
+    Serial.print(incomingChar);
     switch (incomingChar) {
       case 0x0d:
       case 0x0a:
         // command completed;
         command_string[command_idx] = 0x0;
-        Serial.println(command_string);
+        //Serial.println(command_string);
         command_idx = 0;
         if (parseCommand(command_string)) {
           Serial.println("OK");
@@ -159,53 +172,78 @@ void commandif() {
   }
 }
 
-void phaseChange() {
+ISR(PCINT0_vect) {
+  static byte lastState;
+  byte pinstate, changed;
+  unsigned long now, delta;
+  pinstate = PINB & pin_mask;
+  changed = lastState & ~pinstate;
+  lastState = pinstate;
+  now = millis();
+  for (byte channel = 0; channel < CHANNELS; channel++) {
+    if (changed & 1 << channel) {
+      delta = now - state[channel].timemillis;
+      if (delta > 8 && delta < 12) {
+        state[channel].count++;
+      }
+      if (state[channel].count > CYCLES) {
+        state[channel].off_delay = settings[channel].off_delay*10;
+        state[channel].count = 0;
+      }
+      state[channel].timemillis = now;  
+    }
+  }
+}
+
+ISR(TIMER1_COMPA_vect) {
   unsigned long now = millis();
-  unsigned long delta = now - state[0].timemillis;
-  if (debug) Serial.println(delta);
-  if (delta > 8 && delta < 12) {
-    state[0].on_delay++;
+  for (byte i = 0; i <  CHANNELS; i++) {
+    if (now - state[i].timemillis > 1000) {
+      state[i].count = 0;
+    }
+    if (state[i].off_delay > 0) {
+      state[i].off_delay--;
+    }
   }
-  if (state[0].on_delay > 100) {
-    state[0].off_delay = settings[0].off_delay*10;
-    state[0].on_delay = 0;
-  }
-  state[0].timemillis = now;
 }
 
 void setup() {
   Serial.begin(57600);
   Serial.println("Starting...");
   readEEPROM();
-  pinMode(3,INPUT_PULLUP);
-  pinMode(12,OUTPUT);
   pinMode(13,OUTPUT);
-  digitalWrite(12,LOW);
-  digitalWrite(13,LOW);
-  attachInterrupt(1, phaseChange, FALLING);
-  memset(state,0,sizeof(state));
-  debug = 0;
-  for (int i = 0; i < CHANNELS; i++) {
-    state[i].mode = LOW; 
-  }
+  pinMode(13,LOW);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  pinMode(8,INPUT_PULLUP);
+  pinMode(9,INPUT_PULLUP);
+  pin_mask = bits(CHANNELS);
+  PCICR  |= 0b00000001;
+  PCMSK0 |= pin_mask;
+
+    cli(); // отключить глобальные прерывания
+    TCCR1A = 0; // установить регистры в 0
+    TCCR1B = 0; 
+
+    OCR1A = 15624; // установка регистра совпадения
+    TCCR1B |= (1 << WGM12); // включение в CTC режим
+
+    // Установка битов CS10 и CS12 на коэффициент деления 1024
+    TCCR1B |= (1 << CS10);
+    TCCR1B |= (1 << CS12);
+
+    TIMSK1 |= (1 << OCIE1A);  // включение прерываний по совпадению
+    sei(); // включить глобальные прерывания
+
 }
 
 void loop() {
-  if (millis() - state[0].timemillis > 1000) {
-    state[0].on_delay = 0;
+  byte m0,n1;
+  for (byte i = 0; i < CHANNELS; i++) {
+    state[i].mode = ~bitRead(settings[i].mode,1) & (bitRead(settings[i].mode,0) | state[i].off_delay > 0);
   }
-  if (settings[0].mode == 1) {
-    state[0].off_delay = 1;
-  } else if (settings[0].mode == 2) {
-    state[0].off_delay = 0;
-  }
-  if (state[0].off_delay > 0) {
-    state[0].mode = HIGH;
-    state[0].off_delay--;
-  } else {
-    state[0].mode = LOW;
-  }
-  digitalWrite(13,state[0].mode);
   commandif();
   delay(99);
 }
