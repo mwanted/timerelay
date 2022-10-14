58a9e387980d482d17fd3d4085098f8f97daa373
Gleb Belyakov <rem@mwnt.ru>
2022-10-14 12:48:04 +0300

    Some error handling

--- api/app/api.py
+++ api/app/api.py
@@ -4,83 +4,92 @@ import time
 import urllib
 import json
 
-ser = serial.Serial(
-    port='COM3',
-    baudrate=57600,
-    timeout=1
-)
-print(ser.isOpen())
 
-"""
-A: guard_time: 2
-A: short_time: 10
-A: off_delay:  600
-A: mode:       2
-B: guard_time: 2
-B: short_time: 10
-B: off_delay:  600
-B: mode:       2
-"""
-
-
+try: 
+	ser = serial.Serial(
+		port='COM3',
+		baudrate=57600,
+		timeout=1
+	)
+except serial.serialutil.SerialException as e:
+	print(e)
+	exit()
+	
 def chat(command):
-	ser.reset_input_buffer()
-	ser.write((command+'\n').encode('ASCII'))
-	ser.flush()
-	time.sleep(0.5)
-	readed = ser.read(size=200)
-	answer = readed.decode('ASCII')
+	try:
+		ser.reset_input_buffer()
+		ser.write((command+'\n').encode('ASCII'))
+		ser.flush()
+		time.sleep(0.1)
+		answer = ser.read(size=200).decode('ASCII')
+	except serial.serialutil.SerialTimeoutException as e:
+		print(e)
+		exit()
 	data = dict()
 	data["result"] = "failed"
-	data["channels"] = dict()
 	for idx,string in enumerate(answer.splitlines()):
-		if string == '':
-			continue  
 		if string == command:
 			data["command"] = string
 		if string == 'OK':
 			data["result"] = "success"
 		if string[1] == ':':
+			if "channels" not in data: data["channels"] = dict()
 			if string[0] not in data["channels"].keys():
 				data["channels"][string[0]] = dict()
 			data["channels"][string[0]][string.split(':')[1].strip()] = string.split(':')[2].strip()
 	return data
 
+def reportJSONError(e):
+	rm = dict()
+	rm["result"] = "failed"
+	rm["msg"] = e.msg 
+	rm["pos"] = e.pos 
+	rm["doc"] = e.doc
+	return rm
 
+def reportSyntxError(e):
+	rm = dict()
+	rm["result"] = "failed"
+	rm["msg"] = "channel field must be specified" 
+	return rm
+	
 @route('/api/v1/state')
 def getState():
 	return chat('sg')
 
 @route('/api/v1/properties')
 def getProps():
-	return chat('sg')
+	return chat('pg')
 
 @post('/api/v1/properties')
 def setProps():
+	rm = dict()
 	try:
 		data = json.loads(request.body.read().decode('utf-8'))
-		if "write" in data.keys():
-			return chat('pw')
-		if "mode" in data.keys():
-			return chat('ps' + data["channel"] + "m" + data["mode"])
-		if "short_time" in data.keys():
-			return chat('ps' + data["channel"] + "s" + data["short_time"])
-		if "guard_time" in data.keys():
-			return chat('ps' + data["channel"] + "g" + data["guard_time"])
-		if "delay_time" in data.keys():
-			return chat('ps' + data["channel"] + "d" + data["delay_time"])
-		return '{"result":"failed"}'
-	except:
-		return False
+		if "channel" not in data.keys():
+			return reportSyntxError(data)
+		rm["channel"] = data["channel"]
+		for key in data.keys():
+			if key == "channel":    continue
+			if key == "mode":       rm["mode"]       = chat('ps' + data["channel"] + "m" + str(data["mode"]))
+			if key == "short_time": rm["short_time"] = chat('ps' + data["channel"] + "s" + str(data["short_time"]))
+			if key == "guard_time": rm["guard_time"] = chat('ps' + data["channel"] + "g" + str(data["guard_time"]))
+			if key == "delay_time": rm["delay_time"] = chat('ps' + data["channel"] + "d" + str(data["delay_time"]))
+			if key == "write":      rm["write"]      = chat('pw')
+		rm["result"] = "success"
+		return rm 
+	except json.JSONDecodeError as e:
+		return reportJSONError(e)
 		 
 
 @post('/api/v1/state')
 def setState():
 	try:
 		data = json.loads(request.body.read().decode('utf-8'))
-#		command = 'ss' + data["channel"] + data["time"]
-		return chat('ss' + data["channel"] + data["time"])
-	except:
-		return False
+		if "channel" not in data.keys():
+			return reportSyntxError(data)
+		return chat('ss' + data["channel"] + str(data["time"]))
+	except json.JSONDecodeError as e:
+		return reportJSONError(e)
 
 run(host='localhost', port=8088, debug=True)
